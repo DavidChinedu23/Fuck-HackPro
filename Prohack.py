d978d24e6ebd05618cfde0dba2b6f34254217237
David Chinedu <93946668+DavidChinedu23@users.noreply.github.com>
2022-08-11 06:45:10 +0100

    Update Prohack.py

--- Prohack.py
+++ Prohack.py
@@ -1,15 +1,14 @@
-import platform
-import os
-os.system('termux-setup-storage')
-os.system('git pull')
-try:os.system('touch .prox.txt')
-except:pass
-try:os.system('touch .proxy.txt')
-except:pass
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("latter")._site_view_()
-elif 'aarch' in arc:
-	__import__("Filpp").ninex()
-else:
-	exit(f' Unknow device machine {arc}')
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Filpp import maping
+    maping()
+elif bit == '32bit':
+    from latter import maping
+    maping()
